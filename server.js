b56e7d124b8881f9edb46442e1e4d554ee6ad14b
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2025-03-09 14:41:25 +0100

    connect to DB and update start server function

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8081;
 
+app.use(express.json());
+
+//Simple get req to verify if server is online
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello There! - Obi-Wan, aaahhhh general kanobi!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+//Start server function
+const startServer = (port) => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server up and running on port ${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit();
+  }
+};
+
+//server connection starts here
+mongoose.connect(process.env.DATABASE_URL).then(() => {
+  startServer(port);
 });
