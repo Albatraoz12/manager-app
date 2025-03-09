d77e57c38d0de7c20f196c11b7adec0f036cc610
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2025-03-09 17:49:28 +0100

    create route and controller for edit a task and deleteing it

--- backend/controller/taskController.js
+++ backend/controller/taskController.js
@@ -44,4 +44,60 @@ const getTasks = async (req, res) => {
   }
 };
 
-module.exports = { createTask, getTasks };
+const updateTask = async (req, res) => {
+  try {
+    const validUser = await User.findOne({ _id: req.userId });
+    const id = req.params.lid;
+    if (validUser) {
+      const task = await Task.findOne({ _id: id });
+      if (task.user == validUser.id) {
+        const options = { new: true };
+        const editTask = await Task.findByIdAndUpdate(id, req.body, options);
+        res.status(200).json({
+          message: 'List with ID ' + id + ' has now been updated!',
+        });
+      } else {
+        res.status(403).json({
+          ErrorMessage: 'You are not the owner!',
+        });
+      }
+    } else {
+      res.status(400).json({ ErrorMessage: 'You are not an user!' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Find the task
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+
+    // Check if the task belongs to the user
+    if (task.user.toString() !== req.userId) {
+      return res
+        .status(403)
+        .json({ error: 'You are not the owner of this task!' });
+    }
+
+    // Delete the task
+    await Task.deleteOne({ _id: id });
+
+    res.status(200).json({ message: `${task.title} has now been deleted!` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Invalid Id or server error!' });
+  }
+};
+
+module.exports = { createTask, getTasks, updateTask, deleteTask };
--- backend/routes/TaskRoute.js
+++ backend/routes/TaskRoute.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const { authorization } = require('../authorization/auth');
-const { createTask, getTasks } = require('../controller/taskController');
+const {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+} = require('../controller/taskController');
 const router = express.Router();
 
 //@desc Creates a Task for a user
@@ -12,5 +17,15 @@ router.post('/createTask/:uid', authorization, createTask);
 //@routes GET /task/getTasks/:id
 //@access Private
 router.get('/getTasks/:uid', authorization, getTasks);
+
+//@desc Validates if users ID from token == task.user and then updates the task
+//@routes PUT /task/editTask/:lid
+//@access Private
+router.put('/editTask/:lid', authorization, updateTask);
+
+//@desc Validates if users ID from token == task.user and then deletes it
+//@routes Delete /:id
+//@access Private
+router.delete('/:id', authorization, deleteTask);
 
 module.exports = router;
