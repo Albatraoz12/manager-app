523cb5ca6cb5bb8beb5a0b323e508883e6019f33
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2025-03-10 20:15:25 +0100

    install input component and finish sign in component

---
+++ frontend/app/signin/page.tsx
@@ -0,0 +1,89 @@
+'use client';
+import React from 'react';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  password: z.string().min(5).max(50),
+});
+
+export default function page() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <section className='md:w-[600px] border bg-gray-700 text-white my-10 p-4 mx-auto py-10 flex flex-col'>
+      <h1 className='text-4xl my-4'>Sign in</h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='space-y-8 flex flex-col'
+        >
+          <FormField
+            control={form.control}
+            name='username'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder='task@manager.com' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder='*******************' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type='submit'
+            className='bg-black text-white hover:cursor-pointer'
+          >
+            Submit
+          </Button>
+        </form>
+        <p className='text-center'>
+          Do you need to create an account?{' '}
+          <Link href={'/signup'}>Sign up here</Link>
+        </p>
+      </Form>
+    </section>
+  );
+}
