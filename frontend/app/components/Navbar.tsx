49cf8c69f3aa5a7735f4b3d4ac67e253bf9a3ff5
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2025-03-10 19:55:43 +0100

    Create navbar component and add it to page

---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className='flex justify-between px-4 py-4 bg-black text-white'>
+      <div>
+        <Link href={'/'}>
+          <h1>Task Manager</h1>
+        </Link>
+      </div>
+      <div>
+        <ul className='flex flex-row gap-3'>
+          <li>
+            <Link href={'/'}>Home</Link>
+          </li>
+          <li>
+            <Link href={'/signin'}>Sign in</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
